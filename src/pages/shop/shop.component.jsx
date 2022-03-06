ce4c862417072d2cd382c860f2ccb6ee87149904
And4Web <anands2807@gmail.com>
2022-03-05 21:34:22 -0800

    header component added

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -15,8 +15,6 @@ class ShopPage extends React.Component {
     const { collections } = this.state;
     return (
       <div className="shop-page">
-        <h1>ShopPage</h1>
-
         {collections.map(({ id, ...otherCollectionProps }) => (
           <CollectionPreview key={id} {...otherCollectionProps} />
         ))}
