de5e0645c0a2310a46d90113e5a829725f11f1aa
And4Web <anands2807@gmail.com>
2022-03-05 17:38:21 -0800

    collection-preview component added

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,27 +1,28 @@
-import React from 'react';
-import SHOP_DATA from './shop.data';
-import CollectionPreview from '../../components/preview-collection/preview-collection.component';
+import React from "react";
+import SHOP_DATA from "./shop.data";
+import CollectionPreview from "../../components/preview-collection/preview-collection.component";
 
-class ShopPage extends React.Component{
-  constructor(props){
+class ShopPage extends React.Component {
+  constructor(props) {
     super(props);
 
     this.state = {
-      collections: SHOP_DATA
+      collections: SHOP_DATA,
     };
   }
 
   render() {
-    const {collections} = this.state;
-    return (<div className='shop-page'>
-    {
-      collections.map(({id, ...otherCollectionProps}) => (
-        <CollectionPreview key={id} {...otherCollectionProps}/>
-      ))
-    }
+    const { collections } = this.state;
+    return (
+      <div className="shop-page">
+        <h1>ShopPage</h1>
 
-    </div>);
-    }
+        {collections.map(({ id, ...otherCollectionProps }) => (
+          <CollectionPreview key={id} {...otherCollectionProps} />
+        ))}
+      </div>
+    );
   }
+}
 
 export default ShopPage;
