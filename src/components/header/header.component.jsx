6bd5bfee7b75d8d50aef1af21b2be82b7108be30
And4Web <anands2807@gmail.com>
2022-03-07 08:40:33 -0800

    sign out feature added.

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
+import { auth } from "../../firebase/firebase-utils";
 import "./header.styles.scss";
 
-function Header() {
+function Header({ currentUser }) {
   return (
     <div className="header">
       <Link to="/" className="logo-container">
@@ -17,6 +18,15 @@ function Header() {
         <Link to="/contact" className="option">
           CONTACT
         </Link>
+        {currentUser ? (
+          <div className="option" onClick={() => auth.signOut()}>
+            SIGN OUT{" "}
+          </div>
+        ) : (
+          <Link className="option" to="/signin">
+            SIGN IN
+          </Link>
+        )}
       </div>
     </div>
   );
