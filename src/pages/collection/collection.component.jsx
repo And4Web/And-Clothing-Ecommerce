39596cd936508fc6570255d31885a1ec787b23eb
And4Web <anands2807@gmail.com>
2022-03-23 19:13:01 +0530

    nested routing and selector created, used ownProps as second parameter to map state to props

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,7 +1,10 @@
 import React from "react";
-// import collectionItem from "../../components/collection-item/collection-item.component";
+import { connect } from "react-redux";
+import collectionItem from "../../components/collection-item/collection-item.component";
 
 import "./collection.styles.scss";
+
+import { selectCollection } from "../../redux/shop/shop.selectors";
 
 const CollectionPage = ({ match }) => (
   <div className="collection-page">
@@ -9,4 +12,8 @@ const CollectionPage = ({ match }) => (
   </div>
 );
 
-export default CollectionPage;
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state),
+});
+
+export default connect(mapStateToProps)(CollectionPage);
