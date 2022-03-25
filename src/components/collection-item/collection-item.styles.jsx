8e323d29627e06cd2961a984f6892acad4ee6386
And4Web <anands2807@gmail.com>
2022-03-25 18:20:42 +0530

    trying to make collection item component a styled component

---
+++ src/components/collection-item/collection-item.styles.jsx
@@ -0,0 +1,11 @@
+import styled, { css } from "styled-components";
+
+const CollectionItemContainer = styled.div`
+  width: 22vw;
+  display: flex;
+  flex-direction: column;
+  height: 350px;
+  align-items: center;
+  position: relative;
+  margin: 10px;
+`;
